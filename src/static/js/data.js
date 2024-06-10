7684d656a3283a78cfe6455f1dfd737cf4b805d0
edevPires <133178329+edevPires@users.noreply.github.com>
2024-06-10 09:40:36 -0300

    Comitada mosntruosa

---
+++ src/static/js/data.js
@@ -0,0 +1,44 @@
+const casas = [
+    {
+        id:'1',
+        casa: 'Casa 1',
+        img: 'https://images.unsplash.com/photo-1570129477492-45c003edd2be?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2FzYXxlbnwwfHwwfHx8MA%3D%3D',
+        estrelas: '⭐⭐⭐⭐⭐',
+        descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A exautem aliquid provident nesciunt fuga harum minus ipsumdolorum voluptate eveniet ipsa, voluptas architecto magni nihil repellendus voluptatum quos consequatur!'
+    },
+    {
+        id:'2',
+        casa: 'Casa 2',
+        img: 'https://images.unsplash.com/photo-1568605114967-8130f3a36994?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2FzYXxlbnwwfHwwfHx8MA%3D%3D',
+        estrelas: '⭐⭐⭐⭐',
+        descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A exautem aliquid provident nesciunt fuga harum minus ipsumdolorum voluptate eveniet ipsa, voluptas architecto magni nihil repellendus voluptatum quos consequatur!'
+    },
+    {
+        id:'3',
+        casa: 'Casa 3',
+        img: 'https://images.unsplash.com/photo-1576941089067-2de3c901e126?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGNhc2F8ZW58MHx8MHx8fDA%3D',
+        estrelas: '⭐⭐⭐',
+        descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A exautem aliquid provident nesciunt fuga harum minus ipsumdolorum voluptate eveniet ipsa, voluptas architecto magni nihil repellendus voluptatum quos consequatur!'
+    },
+    {
+        id:'4',
+        casa: 'Casa 4',
+        img: 'https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?q=80&w=1475&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        estrelas: '⭐⭐⭐',
+        descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A exautem aliquid provident nesciunt fuga harum minus ipsumdolorum voluptate eveniet ipsa, voluptas architecto magni nihil repellendus voluptatum quos consequatur!'
+    },
+    {
+        id:'5',
+        casa: 'Casa 5',
+        img: 'https://plus.unsplash.com/premium_photo-1661915661139-5b6a4e4a6fcc?q=80&w=1567&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        estrelas: '⭐⭐⭐',
+        descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A exautem aliquid provident nesciunt fuga harum minus ipsumdolorum voluptate eveniet ipsa, voluptas architecto magni nihil repellendus voluptatum quos consequatur!'
+    },
+    {
+        id:'6',
+        casa: 'Casa 6',
+        img: 'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        estrelas: '⭐⭐⭐',
+        descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A exautem aliquid provident nesciunt fuga harum minus ipsumdolorum voluptate eveniet ipsa, voluptas architecto magni nihil repellendus voluptatum quos consequatur!'
+    },
+]
--- src/static/js/script.js
+++ src/static/js/script.js
@@ -37,3 +37,55 @@ sidebarClose.addEventListener('click', () => {
     hideShowSidebar('hide');
 })
 
+const modal = document.querySelector('.modal')
+const modalBg = document.querySelector('.modal-bg')
+
+const saibaBtn = document.querySelectorAll('.saiba')
+saibaBtn.forEach(element => {
+    element.addEventListener('click', ()=>{
+        modal.innerHTML = ''
+        modalBg.classList.toggle('hidden')
+        modal.classList.toggle('hidden')
+        var id = element.id
+        casas.forEach(element => {
+            if(element.id === id) {
+                const htmlInsert = `
+                    <div class="flex h-full flex-grow border-r justify-center items-center">
+                        <div
+                            class="w-3/4 bg-slate-900  h-[300px] rounded-xl bg-cover bg-center"
+                            style="
+                                background-image: url(${element.img});
+                            "
+                        ></div>
+                    </div>
+                    <div class="flex h-full flex-grow max-w-[50%] justify-center items-center">
+                        <div class="flex flex-wrap px-5 py-5">
+                            <div class="flex justify-between w-full py-3">
+                                <h1 class="text-2xl font-bold">${element.casa}</h1>
+                                <p>${element.estrelas}</p>
+                            </div>
+                            <div>
+                                <p>
+                                    ${element.descricao}
+                                </p>
+                            </div>
+                            <div class="flex justify-center items-center w-full mt-5">
+                                <button
+                                class="text-xl flex justify-center items-center font-bold bg-blue-800 text-white px-6 py-2 rounded-full hover:scale-105 transition-all hover:scale-110 transition-all"
+                                >
+                                Saiba Mais
+                                </button>
+                            </div>
+                            </div>
+                    </div>
+                `
+                modal.innerHTML = htmlInsert
+            }
+        });
+    })
+});
+
+modalBg.addEventListener('click', ()=> {
+    modal.classList.toggle('hidden')
+    modalBg.classList.toggle('hidden')
+})
